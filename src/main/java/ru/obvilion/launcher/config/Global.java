f6e9a35cb395a99df0249a668d3d2806ac648705
KeviTV <zxcvbnm20042005@gmail.com>
2021-09-11 00:16:03 +0300

    chore: new version

--- src/main/java/ru/obvilion/launcher/config/Global.java
+++ src/main/java/ru/obvilion/launcher/config/Global.java
@@ -3,7 +3,8 @@ package ru.obvilion.launcher.config;
 import java.io.File;
 
 public class Global {
-    public static final String API_LINK = "https://obvilionnetwork.ru/apiv2/";
+    public static final String API_LINK = "https://obvilionnetwork.ru/api/";
+    public static final String VERSION  = "2.7dev";
     public static final File USER_HOME = new File(System.getProperty("user.home"));
     public static final File LAUNCHER_HOME = new File(USER_HOME, ".ObvilionNetwork");
     public static final File LAUNCHER_CONFIG = new File(LAUNCHER_HOME, "config.properties");
@@ -14,6 +15,4 @@ public class Global {
     public static String JAVA_BIN = JAVA_HOME +
             File.separator + "bin" +
             File.separator + "java";
-
-    public static boolean isAuthorised = false;
 }
